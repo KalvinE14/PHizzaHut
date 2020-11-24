13d3af70ddcbca338be691fbe57b5508c9993278
jonathanchrs <61592558+jonathanchrs@users.noreply.github.com>
2020-11-24 20:26:12 +0700

    add transaction and cart tables

---
+++ PHizza Hut/database/migrations/2020_11_24_115607_create_transaction_headers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionHeadersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_headers', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('userID')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_headers');
+    }
+}
---
+++ PHizza Hut/database/migrations/2020_11_24_125608_create_transaction_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->foreign('transaction_id')->references('id')->on('transaction_headers')->onDelete('cascade');
+            $table->foreignId('transaction_id');
+            $table->foreign('pizza_id')->references('pizzaID')->on('pizzas')->onDelete('cascade');
+            $table->foreignId('pizza_id');
+            $table->integer('quantity');
+            $table->integer('total_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
